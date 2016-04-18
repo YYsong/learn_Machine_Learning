dafaa0e8ae4143103beecf559181f49013491866
songyayong <songyayong@sina.com>
2016-04-18 22:04:13 +0800

    logRegres final

--- ch05/logRegres.py
+++ ch05/logRegres.py
@@ -106,3 +106,46 @@ def plotBestFit(weights):
     plt.xlabel('X1')
     plt.ylabel('X2')
     plt.show()
+
+def classifyVector(inX,weights):
+    #分类方法：输入向量*回归系数，求和，放入sigmoid中
+    prob = sigmoid(sum(inX*weights))
+    if prob > 0.5 : return 1.0
+    else : return 0.0
+
+#训练数据集，得到回归系数，再在测试集上得到错误率并返回，每次运行结果可能不一样
+def colicTest():
+    frTrain = open('horseColicTraining.txt')
+    frTest = open('horseColicTest.txt')
+    trainingSet = [];
+    trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    #获得训练的回归系数
+    trainWeights = stocGradAscent1 (array(trainingSet),trainingLabels,500)
+    
+    errorCount = 0;
+    numTestVec = 0.0
+    #通过测试集得到错误率
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr),trainWeights))!= int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount)/numTestVec)
+    print "the error rate of this test is : %f" % errorRate
+    return errorRate
+
+def multiTest():
+    numTests = 10;errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print "after %d iterations the average error rate is : %f " %(numTests,errorSum/float(numTests))
